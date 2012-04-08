b4821bdab20a5116914dce9c7dcfe435650b37fd
Zoe McCarthy <zoemccarthy12@gmail.com>
2012-04-08 16:44:26 -0500

    tuned serial python for kinect mapper

--- serial_python.py
+++ serial_python.py
@@ -5,9 +5,11 @@ plt.ion()
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 counter = 0
 data_list = []
-for i in range(8):
+for i in range(6):
     data_list.append([])
 fig1 = plt.figure()
+
+labels = ['uL', 'LDC', 'velL', 'uR', 'RDC', 'velR']
 
 while 1:
     fun = ser.readline()
@@ -15,14 +17,25 @@ while 1:
     for i, word in enumerate(fun.split()):
         try:
             print i, word
-            value = int(word)
+            value = float(word)
             data_list[i].append(word)
         except ValueError:
             print "null value???", word
-    if counter % 5 == 0:
+    if counter % 5 == 1:
         fig1.clear()
-        for datas in data_list:
-            plt.plot(datas[-200:])
+        for i, datas in enumerate(data_list):
+            if i == 1 or i == 4:
+                plt.subplot(313)
+                plt.plot(datas[-200:], label=labels[i])
+                plt.legend()
+            elif i == 2 or i == 5:
+                plt.subplot(312)
+                plt.plot(datas[-200:], label=labels[i])
+                plt.legend()
+            elif i == 0 or i == 3:
+                plt.subplot(311)
+                plt.plot(datas[-200:], label=labels[i])
+                plt.legend()
         plt.draw()
 
 
