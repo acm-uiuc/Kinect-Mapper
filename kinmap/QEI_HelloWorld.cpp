0a12cd69c2bf2f3cd1e3615e21f2af6a9f9b76e1
Alex Burck <burck1@illinois.edu>
2013-01-29 19:43:02 -0600

    moar

---
+++ kinmap/QEI_HelloWorld.cpp
@@ -0,0 +1,16 @@
+#include "QEI.h"
+
+Serial pc(USBTX, USBRX);
+//Use X4 encoding.
+//QEI wheel(p29, p30, NC, 624, QEI::X4_ENCODING);
+//Use X2 encoding by default.
+QEI wheel (p29, p30, NC, 624);
+
+int main() {
+
+    while(1){
+        wait(0.1);
+        pc.printf("Pulses is: %i\n", wheel.getPulses());
+    }
+
+}
