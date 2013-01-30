807835bb505873db965915fa2f969e1efb68fc22
Alex Burck <burck1@illinois.edu>
2013-01-29 20:52:52 -0600

    changes

--- kinmap/arduino_DUE_130129.ino
+++
@@ -1,8 +0,0 @@
-// @reference http://arduino.cc/en/Guide/ArduinoDue for DUE stuff
-// @reference http://bit.ly/VoBkia for X2 encoding
-// @reference http://mbed.org/cookbook/QEI for QEI Library
-// @reference http://mbed.org/users/aberk/code/RS-EDP-RDS-Rover/ for rover library
-
-#include <QEI.h>
-
-
