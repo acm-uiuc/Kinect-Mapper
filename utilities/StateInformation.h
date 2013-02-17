eac80b2dfa0718d8f9107cfa19084dd21f1f4f18
bryan301 <brymail301@gmail.com>
2013-02-17 15:05:08 -0600

    Added some necessary structs.

---
+++ utilities/StateInformation.h
@@ -0,0 +1,37 @@
+/************************************************************************
+*************************************************************************
+ Reminder: None of the functions are set in stone, these are just in place
+ to provide guideance on what we will need to implement.  Make whatever 
+ changes you think are necessary.
+ ************************************************************************
+ ***********************************************************************/
+
+#ifndef STATEINFORMATION_H
+#define STATEINFORMATION_H
+
+/** \brief
+ * Stores odometry information about the robot
+ */
+struct OdomTrans 
+{
+  /** \brief
+   * Stores translations where +z is forward, +x is right, and +y is down
+   */
+  double x,y,z;
+
+  /** \brief
+   * Stores the Euler Angles that encodes the robot's pose
+   */
+  double alpha,beta,gamma;
+};
+
+/** \brief
+ * Stores the data about the current state of the robot
+ */
+struct FrameData
+{
+  OdomTrans trans; 
+  
+};
+
+#endif
