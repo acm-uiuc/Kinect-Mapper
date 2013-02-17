e138300add4d94ec817f8015cb81d033921f2e39
bryan301 <brymail301@gmail.com>
2013-02-17 15:04:09 -0600

    Cleaned up some unnecessary files. Added examples of documentation markups in odometry.

--- navigation/navigation.cpp
+++
@@ -1,9 +0,0 @@
-/************************************************************************
-*************************************************************************
- Reminder: None of the functions are set in stone, these are just in place
- to provide guideance on what we will need to implement.  Make whatever 
- changes you think are necessary.
- ************************************************************************
- ***********************************************************************/
-
-#include "navigation.h"
