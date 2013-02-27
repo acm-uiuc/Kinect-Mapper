5cbbd0c7268146e86ed94c9801a1aa15f7a262e7
Sam Laane <laane2@acm.uiuc.edu>
2013-02-26 19:18:33 -0600

    recoved files

---
+++ odometry/odometry.cpp
@@ -0,0 +1,81 @@
+/************************************************************************
+*************************************************************************
+ Reminder: None of the functions are set in stone, these are just in place
+ to provide guideance on what we will need to implement.  Make whatever 
+ changes you think are necessary.
+ ************************************************************************
+ ***********************************************************************/
+
+#include "odometry.h"
+
+RGBDVisOdometry::RGBDVisOdometry(const KinectInter& camera)
+{
+  //TODO: Set up visual odometry options
+  /*fovis::VisualOdometryOptions options = 
+      fovis::VisualOdometry::getDefaultOptions();
+  fovis::Rectification rect(camera.getCameraParameters());
+  Odom_ = new fovis::VisualOdometry(&rect, options);*/
+}
+
+RGBDVisOdometry::~RGBDVisOdometry()
+{
+}
+
+/*************************************************************
+ *
+ *
+ ***********************************************************/
+OdomTrans& RGBDVisOdometry::getMotionEstimate(FrameDataPtr currFrame)
+{/*
+  // Process frame if a new frame is added
+  if (currFrame != NULL)
+    Odom_->processFrame(currFrame->grayImage, currFrame->depthImage);
+
+  // Extract pose
+  Eigen::Isometry3d pose = Odom_->getMotionEstimate();
+
+  // Fill in struct and return requested information
+  return Isometry3DToOdomTrans(pose);*/
+  OdomTrans trans;
+  return trans;
+}
+
+/*************************************************************
+ *
+ *
+ ***********************************************************/
+OdomTrans& RGBDVisOdometry::getPose(FrameDataPtr currFrame)
+{/*
+  // Process frame if a new frame is added
+  if (currFrame != NULL)
+    Odom_->processFrame(currFrame->grayImage, currFrame->depthImage);
+
+  // Extract pose
+  Eigen::Isometry3d pose = Odom_->getPose();
+
+  // Fill in struct and return requested information
+  return Isometry3DToOdomTrans(pose);*/
+  OdomTrans trans;
+  return trans;
+}
+
+/*************************************************************
+ *
+ *
+ ***********************************************************/
+OdomTrans& Isometry3DToOdomTrans(Eigen::Isometry3d info)
+{
+  /*
+  Eigen::Vector3d xyz = m.translation();
+  Eigen::Vector3d rpy = m.rotation().eulerAngles(0, 1, 2);*/
+
+  OdomTrans odomInfo;/*
+  odomInfo.x = xyz(0);
+  odomInfo.y = xyz(1);
+  odomInfo.z = syz(2);
+  odomInfo.alpha = rpy(0);
+  odomInfo.beta = rpy(1);
+  odomInfo.gamma = rpy(2);*/
+
+  return odomInfo;
+}
